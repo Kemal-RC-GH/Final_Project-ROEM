e30417fe276cde6150cd335ab336ae74fc8143aa
Kemal-RC-GH <kemaaal13@gmail.com>
2024-07-19 02:51:48 +0300

    get_events function done!

--- app.py
+++ app.py
@@ -18,5 +18,21 @@ def login_validation(username, password):
         return True
     return False                # if both username and password are correct return true
 
+def get_events(username):
+    events = []                 #initialize empty event
+    
+    try:
+        file = open("events.txt" , "r")
+        for line in file:
+            event_parts = line.strip().split(",")
+            if len(event_parts) == 4:
+                event_id, title, description, date = event_parts 
+                if username == title.split("_")[0]: 
+                    events.append({"id": event_id, "title": title, "description" : description, "date": date})            # title of the event and its username split
+        file.close()
+    except FileNotFoundError:                   # if file is currupt or have problems
+        print("event.txt file not found!")
+    return events                               # we get the event now
+                          
 if __name__ == '__main__':      #part of the flask format
     app.run(debug=True)
