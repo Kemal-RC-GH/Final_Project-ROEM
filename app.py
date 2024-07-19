0648556df53a97dcca9914ed74fc9f4e186800c6
Kemal-RC-GH <kemaaal13@gmail.com>
2024-07-19 10:15:12 +0300

    Index and register html files added, register and login done!

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, flash, get_flashed_messages, json
-from datetime import datetime
+#from datetime import datetime
 
 app = Flask(__name__)           # All required classes and functions imported. Flask file defined
-app.secret_key = '1234@$'       # secret added for the sake of syntax
+app.secret_key = "1234@$"       # secret added for the sake of syntax
 
+users_file_path = "users.json"
 def load_users():
-    file = open("user.json" , "r")
+    file = open(users_file_path , "r")
     users = json.load(file)
-    file.clsoe()
+    file.close()
     return users
+
+def save_users(user):
+    file = open(users_file_path, "w")
+    json.dump(user, file)
+    file.close()                        # registered users are saved in the users.json file    
 
 USERS = load_users()            # admin users loaded from json file
 
@@ -33,6 +39,59 @@ def get_events(username):
     except FileNotFoundError:                   # if file is currupt or have problems
         print("event.txt file not found!")
     return events                               # we get the event now
+
+@app.route("/", methods=["GET"])
+def index():
+    username = session.get("username")
+    if username:
+        return redirect("/login")
+    return render_template("index.html")        # route defined for admin login
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        users = load_users()
+        user_password = users.get(username)
+        if user_password and user_password == password:
+            session["username"] = username
+            return redirect("/dashboard")
+        flash("Invalid username or password!")
+    return render_template("login.html", flash_message=get_flashed_messages())
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        if password != confirm_password:
+            flash("Passwords do not match!")
+            return render_template("register.html", flash_message=get_flashed_messages())
+
+        users = load_users()          # Here we are going to check ifvthe username exists
+        username_exists = False
+        for key in users.keys():
+            if key == username:
+                username_exists = True
+                break
+
+        if username_exists:          # Here if username exists, it will flash a message otherwise will register
+            flash("Username already exists!")
+            return render_template("register.html", flash_message=get_flashed_messages())
+      
+        users[username] = password   # build the new dictionary
+        save_users(users)            # save it to the json file using save_user function
+        session["username"] = username # update session for direct login and flash the below message
+        flash("Registration successful! Please log in.")
+        return redirect("/login")
+    
+    return render_template("register.html", flash_message=get_flashed_messages())
+
+
+
                           
-if __name__ == '__main__':      #part of the flask format
+if __name__ == "__main__":      #part of the flask format
     app.run(debug=True)
